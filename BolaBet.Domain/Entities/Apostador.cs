bb07613700d9cc54abff01b9cbb1cd177ef7fe45
JacksonS.Maciel <jacksons.m2018@gmail.com>
2022-04-02 22:41:26 -0300

    Primeiro commit e criador a classe Apostador

---
+++ BolaBet.Domain/Entities/Apostador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaBet.Domain.Entities
+{
+    public class Apostador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public double Saldo { get; set; }
+    }
+}
