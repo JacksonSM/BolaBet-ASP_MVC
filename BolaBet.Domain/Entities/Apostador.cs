87aa20c5526169e832f55ec96c699728b4d9f55b
JacksonS.Maciel <jacksons.m2018@gmail.com>
2022-04-03 09:38:52 -0300

    Implementado validações em Apostador.

--- BolaBet.Domain/Entities/Apostador.cs
+++ BolaBet.Domain/Entities/Apostador.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BolaBet.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,37 @@ namespace BolaBet.Domain.Entities
         public string Nome { get; set; }
         public string Usuario { get; set; }
         public string Senha { get; set; }
-        public double Saldo { get; set; }
+        public double Saldo { get; private set; }
+
+        public Apostador(string nome, string usuario, string senha)
+        {
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome),
+               "Nome inválido. Nome é obrigatório");
+            DomainExceptionValidation.When(nome.Length > 50,
+                "Nome muito longo. É permitido no máximo 50 caracteres");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(usuario),
+               "Usuário inválido. Usuario é obrigatório");
+            DomainExceptionValidation.When(nome.Length > 30,
+                "Usuário muito longo. É permitido no máximo 30 caracteres"); 
+            
+            DomainExceptionValidation.When(string.IsNullOrEmpty(senha),
+               "Senha inválido. Senha é obrigatório");
+            DomainExceptionValidation.When(nome.Length > 8,
+                "Senha muito longo. É permitido no máximo 8 caracteres");
+
+            Nome = nome;
+            Usuario = usuario;
+            Senha = senha;
+        }
+        public void AcresentarSaldo(double valor)
+        {
+            Saldo += valor;
+        }
+        public void RetirarSaldo(double valor)
+        {
+            Saldo -= valor;
+        }
     }
 }
